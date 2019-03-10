e4fab142c2a6429a90c66709bc4f9e7dfc6b3660
Tomasz Chybzinski <tomasz.chybzinski@fingo.pl>
2019-03-10 19:05:04 +0100

    Loader on books

--- src/components/bookcards/BookCards.js
+++ src/components/bookcards/BookCards.js
@@ -13,42 +13,41 @@ import {
 
 const BookCards = ({ books }) => (
   <div className="book-cards-wrapper">
-    {books &&
-      books.map((book, index) => (
-        <div key={index} className="book-card">
-          <Card>
-            <CardImg
-              top
-              width="100%"
-              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-              alt="Card image cap"
-            />
-            <CardBody>
-              <CardTitle>
+    {books.map((book, index) => (
+      <div key={index} className="book-card">
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>
+              <span className="d-block">
+                <strong>Tytuł</strong>
+              </span>
+              {book.title}
+            </CardTitle>
+            {book.subtitle && (
+              <CardSubtitle>
                 <span className="d-block">
-                  <strong>Tytuł</strong>
+                  <strong>Podtytuł: </strong>
                 </span>
-                {book.title}
-              </CardTitle>
-              {book.subtitle && (
-                <CardSubtitle>
-                  <span className="d-block">
-                    <strong>Podtytuł: </strong>
-                  </span>
-                  {book.subtitle}
-                </CardSubtitle>
-              )}
-              <CardText>
-                <span className="d-block">
-                  <strong>Opis: </strong>
-                </span>
-                {book.description}
-              </CardText>
-              <Button className="book-card-button">Buy</Button>
-            </CardBody>
-          </Card>
-        </div>
-      ))}
+                {book.subtitle}
+              </CardSubtitle>
+            )}
+            <CardText>
+              <span className="d-block">
+                <strong>Opis: </strong>
+              </span>
+              {book.description}
+            </CardText>
+            <Button className="book-card-button">Buy</Button>
+          </CardBody>
+        </Card>
+      </div>
+    ))}
   </div>
 );
 
--- src/components/bookcards/BookCardsContainer.js
+++ src/components/bookcards/BookCardsContainer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import BookCards from "./BookCards";
 import firebase from "../../firebase";
+import Loader from "../loader/Loader";
 
 const BookCardsContainer = () => {
   const [books, setBooks] = useState([]);
@@ -9,7 +10,7 @@ const BookCardsContainer = () => {
     firebase.getBooks().then(data => setBooks(data));
   }, []);
 
-  return <BookCards books={books} />;
+  return <div>{books.length ? <BookCards books={books} /> : <Loader />}</div>;
 };
 
 export default BookCardsContainer;
