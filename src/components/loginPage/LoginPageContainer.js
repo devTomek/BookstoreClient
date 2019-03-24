b54d459eeeedf8c79d5e2d46e9c991dfadb1fc7d
Tomasz Chybzinski <tomasz.chybzinski@fingo.pl>
2019-03-24 15:21:25 +0100

    WIP: login notifications

--- src/components/loginPage/LoginPageContainer.js
+++ src/components/loginPage/LoginPageContainer.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import LoginPage from "./LoginPage";
-import firebase from "./../../firebase";
+import firebase from "../../firebase";
 import passwordValidator from "password-validator";
 import Loader from "../loader/Loader";
+import { createNotification } from "../App";
+import { ERROR, SUCCESS } from "../../constants";
 
 const LoginPageContainer = () => {
     const [backgroundPicture, setBackgroudPicture] = useState("");
@@ -13,60 +15,86 @@ const LoginPageContainer = () => {
     const schema = new passwordValidator();
 
     schema
-        .is().min(6)
-        .is().max(100)
-        .has().uppercase()
-        .has().lowercase()
-        .has().digits()
-        .has().not().spaces()
-        .has().symbols();
+        .is()
+        .min(6)
+        .is()
+        .max(100)
+        .has()
+        .uppercase()
+        .has()
+        .lowercase()
+        .has()
+        .digits()
+        .has()
+        .not()
+        .spaces()
+        .has()
+        .symbols();
 
     useEffect(() => {
         getBackground();
     }, []);
 
-    const getBackground = () => firebase.getBackground()
-        .then(url => {
-            setBackgroudPicture(url);
-            setLoading(false);
-        })
-        .catch(error => console.error(error));
+    const getBackground = () =>
+        firebase
+            .getBackground()
+            .then(url => {
+                setBackgroudPicture(url);
+                setLoading(false);
+            })
+            .catch(error => console.error(error));
 
     const isPasswordValid = () => {
         if (password.length > 0) {
-            return schema.validate(password)
+            return schema.validate(password);
         }
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = e => {
         e.preventDefault();
 
         if (isPasswordValid()) {
             setLoading(true);
 
-            firebase.login(login, password)
+            firebase
+                .login(login, password)
                 .then(errorMessage => {
                     setLoading(false);
                     setErrorMessage(errorMessage);
                 })
-                .catch(error => console.error(error))
+                .catch(error => {
+                    console.error(error);
+                });
         }
     };
 
+    useEffect(() => {
+        // todo
+        errorMessage
+            ? createNotification(ERROR, errorMessage, "Login", 5000)
+            : createNotification(
+                  SUCCESS,
+                  "Zalogowano poprawnie",
+                  "Logowanie",
+                  3000
+              );
+    }, [isLoading, errorMessage]);
+
     return (
         <div>
-            {/* todo: <LoginError/> */}
-            {!errorMessage && <h1 style={{ position: "absolute" }}>Złe hasło lub login</h1>}
-            {isLoading ?
-                <Loader /> :
+            {isLoading ? (
+                <Loader />
+            ) : (
                 <LoginPage
                     backgroundPicture={backgroundPicture}
                     isPasswordValid={isPasswordValid()}
                     handleSubmit={handleSubmit}
                     getLogin={e => setLogin(e.target.value)}
-                    getPassword={e => setPassword(e.target.value)} />}
+                    getPassword={e => setPassword(e.target.value)}
+                />
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default LoginPageContainer;
