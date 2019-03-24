665c9df97f81246b45e217897a794e28b14dcc8d
Tomasz Chybzinski <tomasz.chybzinski@fingo.pl>
2019-03-24 15:21:06 +0100

    react-notifications setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,75 @@
 import React, { useState, useEffect, Suspense, lazy } from "react";
 import Loader from "./loader/Loader";
 import firebase from "../firebase";
+import {
+    NotificationContainer,
+    NotificationManager
+} from "react-notifications";
+import { INFO, SUCCESS, WARNING, ERROR } from "../constants";
 
 const NavigationBarContainer = lazy(() =>
     import("./navigationBar/NavigationBarContainer")
 );
 const BookCardsContainer = lazy(() => import("./bookcards/BookCardsContainer"));
 const FooterContainer = lazy(() => import("./footer/FooterContainer"));
-const LoginPageContainer = lazy(() => import("./loginPage/LoginPageContainer"))
+const LoginPageContainer = lazy(() => import("./loginPage/LoginPageContainer"));
+
+export const createNotification = (
+    type,
+    message,
+    title = "",
+    timeOut = 3000,
+    callback = () => {}
+) => {
+    const result = () => {
+        switch (type) {
+            case INFO:
+                NotificationManager.info(message, title, timeOut, callback);
+                break;
+            case SUCCESS:
+                NotificationManager.success(message, title, timeOut, callback);
+                break;
+            case WARNING:
+                NotificationManager.warning(message, title, timeOut, callback);
+                break;
+            case ERROR:
+                NotificationManager.error(message, title, timeOut, callback);
+                break;
+            default:
+                return null;
+        }
+    };
+
+    return result();
+};
 
 const App = () => {
     const [user, setUser] = useState(null);
-
-    const authListener = () => {
-        firebase.auth.onAuthStateChanged(user => user ? setUser(user) : setUser(null));
-    };
 
     useEffect(() => {
         authListener();
     }, []);
 
+    const authListener = () => {
+        firebase.auth.onAuthStateChanged(user =>
+            user ? setUser(user) : setUser(null)
+        );
+    };
+
     return (
         <Suspense fallback={<Loader />}>
-            {user ?
+            {user ? (
                 <div>
                     <NavigationBarContainer />
                     <BookCardsContainer />
                     <FooterContainer />
-                </div> :
-                <LoginPageContainer />}
+                </div>
+            ) : (
+                <LoginPageContainer />
+            )}
+            <NotificationContainer />
         </Suspense>
     );
-}
+};
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const INFO = "INFO";
+const SUCCESS = "SUCCESS";
+const WARNING = "WARNING";
+const ERROR = "ERROR";
+
+export { INFO, SUCCESS, WARNING, ERROR };
