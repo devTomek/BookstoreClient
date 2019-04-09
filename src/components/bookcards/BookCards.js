b048cd629af7f9c910f09d152ee2ae792cebae38
Tomasz Chybzinski <tomek@MacBook-Pro-Tomasz.local>
2019-04-09 20:40:37 +0200

    give hieght to main wrapper

--- src/components/bookcards/BookCards.js
+++ src/components/bookcards/BookCards.js
@@ -2,58 +2,58 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./BookCards.scss";
 import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button
+    Card,
+    CardImg,
+    CardText,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    Button
 } from "reactstrap";
 
 const BookCards = ({ books, bookPictureUrl }) => (
-  <div className="book-cards-wrapper">
-    {books.map((book, index) => (
-      <div key={index} className="book-card-wrapper">
-        <Card>
-          <CardImg
-            top
-            src={bookPictureUrl}
-            alt="sapek.jpg"
-            className="book-card-image"
-          />
-          <CardBody>
-            <CardTitle>
-              <span className="d-block">
-                <strong>Tytuł</strong>
-              </span>
-              {book.title}
-            </CardTitle>
-            {book.subtitle && (
-              <CardSubtitle>
-                <span className="d-block">
-                  <strong>Podtytuł: </strong>
-                </span>
-                {book.subtitle}
-              </CardSubtitle>
-            )}
-            <CardText>
-              <span className="d-block">
-                <strong>Opis: </strong>
-              </span>
-              {book.description}
-            </CardText>
-            <Button className="book-card-button">Buy</Button>
-          </CardBody>
-        </Card>
-      </div>
-    ))}
-  </div>
+    <div className="book-cards-wrapper">
+        {books.map((book, index) => (
+            <div key={index} className="book-card-wrapper">
+                <Card>
+                    <CardImg
+                        top
+                        src={bookPictureUrl}
+                        alt="sapek.jpg"
+                        className="book-card-image"
+                    />
+                    <CardBody>
+                        <CardTitle>
+                            <span className="d-block">
+                                <strong>Tytuł</strong>
+                            </span>
+                            {book.title}
+                        </CardTitle>
+                        {book.subtitle && (
+                            <CardSubtitle>
+                                <span className="d-block">
+                                    <strong>Podtytuł: </strong>
+                                </span>
+                                {book.subtitle}
+                            </CardSubtitle>
+                        )}
+                        <CardText>
+                            <span className="d-block">
+                                <strong>Opis: </strong>
+                            </span>
+                            {book.description}
+                        </CardText>
+                        <Button className="book-card-button">Buy</Button>
+                    </CardBody>
+                </Card>
+            </div>
+        ))}
+    </div>
 );
 
 BookCards.propTypes = {
-  books: PropTypes.array.isRequired,
-  bookPictureUrl: PropTypes.string.isRequired
+    books: PropTypes.array.isRequired,
+    bookPictureUrl: PropTypes.string.isRequired
 };
 
 export default BookCards;
