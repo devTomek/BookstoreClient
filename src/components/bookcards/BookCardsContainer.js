81948e6df7984be1f08591e3f6bc84be46f049e1
Tomasz Chybzinski <tomek@MacBook-Pro-Tomasz.local>
2019-04-09 20:40:13 +0200

    get rid of loader inside books component

--- src/components/bookcards/BookCardsContainer.js
+++ src/components/bookcards/BookCardsContainer.js
@@ -1,37 +1,28 @@
 import React, { useState, useEffect } from "react";
 import BookCards from "./BookCards";
 import firebase from "../../firebase";
-import Loader from "../loader/Loader";
 
 const BookCardsContainer = () => {
-  const [books, setBooks] = useState([]);
-  const [bookPictureUrl, setBookPictureUrl] = useState("");
+    const [books, setBooks] = useState([]);
+    const [bookPictureUrl, setBookPictureUrl] = useState("");
 
-  useEffect(() => {
-    getBooks();
-  }, []);
+    useEffect(() => {
+        getBooks();
+    }, []);
 
-  useEffect(() => {
-    getBackground();
-  }, []);
+    useEffect(() => {
+        getBackground();
+    }, []);
 
-  const getBooks = () => firebase.getBooks()
-    .then(books => setBooks(books))
-    .catch(error => console.error(error));
+    const getBooks = () => firebase.getBooks()
+        .then(books => setBooks(books))
+        .catch(error => console.error(error));
 
-  const getBackground = () => firebase.getBookPicture()
-    .then(url => setBookPictureUrl(url))
-    .catch(error => console.error(error));
+    const getBackground = () => firebase.getBookPicture()
+        .then(url => setBookPictureUrl(url))
+        .catch(error => console.error(error));
 
-  return (
-    <div>
-      {books.length && bookPictureUrl ? (
-        <BookCards books={books} bookPictureUrl={bookPictureUrl} />
-      ) : (
-          <Loader />
-        )}
-    </div>
-  );
+    return <BookCards books={books} bookPictureUrl={bookPictureUrl} />
 };
 
 export default BookCardsContainer;
