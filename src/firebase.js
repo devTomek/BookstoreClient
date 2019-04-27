2fecf30af2babf99f1e0b0dc0943fc9caff3bad3
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2019-04-27 14:24:05 +0200

    some setup

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,17 @@ import app from "firebase/app";
 import "firebase/firebase-firestore";
 import "firebase/storage";
 import "firebase/auth";
-import { config } from "./firebaseConfig";
 import { createNotification } from "./components/app/App";
 import { SUCCESS, ERROR, WARNING } from "./constants";
+
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
 
 class Firebase {
     constructor() {
